c2fdbc081c125b081523d768c0856ad87cdd36af
mrospond <marekrospond@gmail.com>
2023-04-29 12:17:10 +0200

    spring boot init

---
+++ src/main/java/com/mans/mans/MansApplication.java
@@ -0,0 +1,13 @@
+package com.mans.mans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mans/mans/MansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mans.mans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
