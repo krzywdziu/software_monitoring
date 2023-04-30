c19fbac3eafb5bb31ecaecd7d53b2f3f70d9d29d
mrospond <marekrospond@gmail.com>
2023-04-30 16:02:57 +0200

    Refactor

--- src/main/java/com/mans/mans/entities/AdministratorEntity.java
+++ src/main/java/com/mans/mans/entities/AdministratorEntity.java
@@ -1,7 +1,12 @@
 package com.mans.mans.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "administrator")
 public class AdministratorEntity {
@@ -25,52 +30,4 @@ public class AdministratorEntity {
 
     @Column(name = "phone_number")
     private String phoneNumber;
-
-    public int getAdministratorId() {
-        return administratorId;
-    }
-
-    public void setAdministratorId(int administratorId) {
-        this.administratorId = administratorId;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
