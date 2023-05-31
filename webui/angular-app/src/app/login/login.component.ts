ad25b3c06553f18b7435a46c50509ab4e2c9c6b5
zpietrza <zpietrza@akamai.com>
2023-05-31 15:23:52 +0200

    Revert "jwt token added to header"
    This reverts commit 0a0ca4649f256e302139df0e21d1f56f63c32188.

--- webui/angular-app/src/app/login/login.component.ts
+++ webui/angular-app/src/app/login/login.component.ts
@@ -4,35 +4,35 @@ import {Router} from "@angular/router";
 import {AuthService} from "../auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-  public form = new FormGroup({
-    username: new FormControl('admin', Validators.required),
-    password: new FormControl('admin', Validators.required)
-  });
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  submitForm() {
-    if (this.form.invalid) {
-      window.alert('Wrong credentials');
-      return;
-    }
-
-    const loginData = {
-      username: this.form.controls.username.value,
-      password: this.form.controls.password.value
-    };
-
-    this.authService.login(loginData).subscribe(() => {
-      this.router.navigate(['/alert/all']);
-    }, (error) => {
-      console.log(error);
-      window.alert('Login failed');
+    public form = new FormGroup({
+        username: new FormControl('admin', Validators.required),
+        password: new FormControl('admin', Validators.required)
     });
-  }
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    submitForm() {
+        if (this.form.invalid) {
+            window.alert('Wrong credentials');
+            return;
+        }
+
+        const loginData = {
+            username: this.form.controls.username.value,
+            password: this.form.controls.password.value
+        };
+
+        this.authService.login(loginData).subscribe(() => {
+            this.router.navigate(['/alert/all']);
+        }, (error) => {
+            console.log(error);
+            window.alert('Login failed');
+        });
+    }
 }
