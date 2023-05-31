22d340b8d4d88f267b5adf1cf612c5bd2ec05cc2
zpietrza <zpietrza@akamai.com>
2023-05-31 16:02:52 +0200

    jwt token added to header

--- webui/angular-app/src/app/servicemen-dashboard/servicemen-dashboard.component.ts
+++ webui/angular-app/src/app/servicemen-dashboard/servicemen-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import {AuthService} from "../auth.service";
 
 
 @Component({
@@ -11,15 +12,20 @@ import { ActivatedRoute } from '@angular/router';
 export class ServicemenDashboardComponent implements OnInit {
   public servicemen: any[] = [];
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthService) {}
 
   // todo: zmienic url gdy bedzie endpoint
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.http.get<any[]>('http://localhost:8080/user/all').subscribe(
-        res => this.servicemen = res,
-        err => console.log('Error: ', err)
+    const token = this.authService.token;
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', token);
+      const params = this.route.snapshot.params;
+
+      this.http.get<any[]>('http://localhost:8080/user/all', { headers, params, responseType: 'json' })
+          .subscribe(
+          res => this.servicemen = res,
+          err => console.log('Error: ', err)
       )
-    })
+    }
   }
 }
--- webui/angular-app/src/app/single-alert/single-alert.component.ts
+++ webui/angular-app/src/app/single-alert/single-alert.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-single-alert',
@@ -10,16 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class SingleAlertComponent implements OnInit{
   public alert: any = {};
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    const token = this.authService.token;
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', token);
+      const params = this.route.snapshot.params;
       const id = params['id'];
 
-      this.http.get<any>(`http://localhost:8080/alert/${id}`).subscribe(
-        res => this.alert = res,
-        err => console.log('Error: ', err)
-      )
-    })
+      this.http.get<any>(`http://localhost:8080/alert/${id}`, {headers, params, responseType: 'json'})
+          .subscribe(
+              res => this.alert = res,
+              err => console.log('Error: ', err)
+          )
+    }
   }
 }
