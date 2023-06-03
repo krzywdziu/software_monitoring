06dee7936f5567f91f22a9259ba4619a1e74a47f
mrospond <marekrospond@gmail.com>
2023-06-03 22:12:07 +0200

    feat: auth user id

--- server/src/main/java/com/zstwp/mans/dto/LoginDto.java
+++ server/src/main/java/com/zstwp/mans/dto/LoginDto.java
@@ -1,19 +1,5 @@
 package com.zstwp.mans.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class LoginDto {
-
-    @NotBlank
-    @Email
-    private String email;
-
-    @NotBlank(message = "Password is mandatory")
-    private String password;
+public record LoginDto(long id, String token) {
 
 }
