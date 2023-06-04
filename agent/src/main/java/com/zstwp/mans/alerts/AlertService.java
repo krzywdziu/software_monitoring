c1030ee1308473b128dbfea2736af4f90815902f
Patryk Krzywdziński <51056034+krzywdziu@users.noreply.github.com>
2023-06-04 18:41:16 +0200

    Update AlertService.java
    Searching logs from directory; categorizing alerts by severity

--- agent/src/main/java/com/zstwp/mans/alerts/AlertService.java
+++ agent/src/main/java/com/zstwp/mans/alerts/AlertService.java
@@ -27,54 +27,147 @@ public class AlertService {
     public void startScanning() {
         System.out.println("test_controller");
 
-        //periodic function; invoking every 5 sec
+         //periodic function; invoking every 5 sec
         Runnable helloRunnable = new Runnable() {
             public void run() {
 
-//
-                try { 
-                    File file = new File(System.getProperty("user.dir") +
-                            "/agent/src/main/java/com/zstwp/mans/alerts/logs.txt");
+                try {
+                    System.out.println(System.getProperty("user.dir") + "\\agent\\src\\main\\java\\com\\zstwp\\mans\\logs");
+                    File directoryPath = new File(System.getProperty("user.dir") + "\\agent\\src\\main\\java\\com\\zstwp\\mans\\logs");
+                    //List of all files and directories
+                    File filesList[] = directoryPath.listFiles();
+                    System.out.println("List of files and directories in the specified directory:");
+                    for(File file : filesList) {
+                        System.out.println("File name: "+file.getName());
 
-                    Scanner scanner = new Scanner(file);
-                    while (scanner.hasNextLine()) {
-                        String data = scanner.nextLine();
 
-                        // using pattern with flags
-                        Pattern pattern = Pattern.compile("PROTERR", Pattern.CASE_INSENSITIVE);
+                        File file_log = new File(System.getProperty("user.dir") + "/agent/src/main/java/com/zstwp/mans/logs/" + file.getName());
 
-                        Matcher matcher = pattern.matcher(data);
+                        Scanner myReader = new Scanner(file_log);
 
-                        // using Matcher find()
-                        while (matcher.find()) {
-                            System.out.println("Found the text \"" + matcher.group()
-                                    + "\" starting at " + matcher.start()
-                                    + " index and ending at index " + matcher.end());
-                            
-                            System.out.println(data);
-                            
-                            AlertStatus status = AlertStatus.UNASSIGNED;
+                        while (myReader.hasNextLine()) {
+                            String data = myReader.nextLine();
 
-                            LocalDateTime time = LocalDateTime .now();
+                            // using pattern with flags
+                            Pattern pattern_err = Pattern.compile("ERROR", Pattern.CASE_INSENSITIVE);
 
-                            AlertSeverity severity = AlertSeverity.ERROR;
+                            Matcher matcher_err = pattern_err.matcher(data);
 
-                            InetAddress.getLocalHost();
+                            Pattern pattern_warn = Pattern.compile("WARNING", Pattern.CASE_INSENSITIVE);
 
-                            AlertDto alert = new AlertDto(data,status,InetAddress.getLocalHost().toString(),severity,time);
+                            Matcher matcher_warn = pattern_warn.matcher(data);
 
-                            sendMessage(alert.toString());
+                            Pattern pattern_not = Pattern.compile("NOTICE", Pattern.CASE_INSENSITIVE);
+
+                            Matcher matcher_not = pattern_not.matcher(data);
+
+                            Pattern pattern_crit = Pattern.compile("CRITICAL", Pattern.CASE_INSENSITIVE);
+
+                            Matcher matcher_crit = pattern_crit.matcher(data);
+
+                            //TASK multiple patterns in one while
+
+                            // using Matcher find()
+                            while (matcher_err.find()) {
+                                System.out.println("Found the text \"" + matcher_err.group()
+                                        + "\" starting at " + matcher_err.start()
+                                        + " index and ending at index " + matcher_err.end());
+                                System.out.println(data + "new");
+
+                                //alert to server
+
+                                AlertStatus status = AlertStatus.UNASSIGNED;
+
+                                LocalDateTime time = LocalDateTime .now();
+
+                                AlertSeverity severity = AlertSeverity.ERROR;
+
+                                InetAddress.getLocalHost();
+
+                                AlertDto alert = new AlertDto(data,status,InetAddress.getLocalHost().toString(),severity,time);
+
+
+                                kafkaProducer.writeMessage(alert.toString());
+
+                            }
+
+                            while (matcher_warn.find()) {
+                                System.out.println("Found the text \"" + matcher_warn.group()
+                                        + "\" starting at " + matcher_warn.start()
+                                        + " index and ending at index " + matcher_warn.end());
+                                System.out.println(data + "new");
+
+                                //alert to server
+
+                                AlertStatus status = AlertStatus.UNASSIGNED;
+
+                                LocalDateTime time = LocalDateTime .now();
+
+                                AlertSeverity severity = AlertSeverity.WARNING;
+
+                                InetAddress.getLocalHost();
+
+                                AlertDto alert = new AlertDto(data,status,InetAddress.getLocalHost().toString(),severity,time);
+
+                                kafkaProducer.writeMessage(alert.toString());
+
+                            }
+
+                            while (matcher_not.find()) {
+                                System.out.println("Found the text \"" + matcher_not.group()
+                                        + "\" starting at " + matcher_not.start()
+                                        + " index and ending at index " + matcher_not.end());
+                                System.out.println(data + "new");
+
+                                //alert to server
+
+                                AlertStatus status = AlertStatus.UNASSIGNED;
+
+                                LocalDateTime time = LocalDateTime .now();
+
+                                AlertSeverity severity = AlertSeverity.NOTICE;
+
+                                InetAddress.getLocalHost();
+
+                                AlertDto alert = new AlertDto(data,status,InetAddress.getLocalHost().toString(),severity,time);
+
+                                kafkaProducer.writeMessage(alert.toString());
+
+                            }
+
+                            while (matcher_crit.find()) {
+                                System.out.println("Found the text \"" + matcher_crit.group()
+                                        + "\" starting at " + matcher_crit.start()
+                                        + " index and ending at index " + matcher_crit.end());
+                                System.out.println(data + "new");
+
+                                //alert to server
+
+                                AlertStatus status = AlertStatus.UNASSIGNED;
+
+                                LocalDateTime time = LocalDateTime .now();
+
+                                AlertSeverity severity = AlertSeverity.WARNING;
+
+                                InetAddress.getLocalHost();
+
+                                AlertDto alert = new AlertDto(data,status,InetAddress.getLocalHost().toString(),severity,time);
+
+                                kafkaProducer.writeMessage(alert.toString());
+
+                            }
                         }
+                        myReader.close();
                     }
-                    scanner.close();
                 } catch (Exception e) {
+                    System.out.println("An error occurred.");
                     e.printStackTrace();
                 }
             }
         };
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        executor.scheduleAtFixedRate(helloRunnable, 0, 5, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(helloRunnable, 0, 60, TimeUnit.SECONDS);
     }
     
     public void sendMessage(String data) {
