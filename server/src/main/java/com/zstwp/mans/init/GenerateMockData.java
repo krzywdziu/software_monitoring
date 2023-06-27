7d3f3af7311bff29165bef098add308cd50015da
mrospond <marekrospond@gmail.com>
2023-06-28 00:19:24 +0200

    chore: init alert description

--- server/src/main/java/com/zstwp/mans/init/GenerateMockData.java
+++ server/src/main/java/com/zstwp/mans/init/GenerateMockData.java
@@ -59,6 +59,27 @@ public class GenerateMockData {
             List<User> users = new ArrayList<>();
             List<Alert> alerts = new ArrayList<>();
 
+            List<String> exampleDescriptions = List.of(
+                    "constraint 'uk_r43af9ap4edm43mmtq01oddj6' of relation 'sers' does not exist",
+                    "rsvp_flow_stateMachine: state SESSIONED does not expect event PATHTEAR",
+                    "mailslot_create: setsockopt(MCAST_ADD) failed - EDC8116I Address not available.",
+                    "Connection limit of 500 inbound connections reached for host 192.168.42.192",
+                    "Unable to categorize 'example.com' by Kerio Web Filter. DNS response 'FAILURE: Invalid authorization' to query 'example.com.f836.ko-34554.v3.url.zvelo.com' is invalid",
+                    "License update failed: Automatic license update failed. User interaction is required by registration server",
+                    "Thread: Critical error detected c0000374",
+                    "最近在探索应用webassembly技术，将之前项目的Typescript写的一个模块改为c/c++实现",
+                    "Caused by: org.hibernate.LazyInitializationException: failed to lazily initialize a collection of role: com.example: could not initialize proxy - no Session",
+                    "Error Handling via Dead Letter Queue in Apache Kafka",
+                    "The remote server returned an error (500) Internal Server Error",
+                    "VPN client error: Error 912",
+                    "EvaluatorTimestampFormatPatternDuplicateFields:887",
+                    "NetworkManager: definition eth0 is not for us (backend 1): eth0 not found in {}",
+                    "src/parse.c:1120:handle_gateway4: assertion failed (scalar(node) == cur_netdef->gateway4): ('correct_gateway' == 'some_random_ip')",
+                    "[ERRO][UPF][Main] UPF Cli Run Error: open Gtp5g: open link: create: operation not supported",
+                    "free5GC does not work after heal operation because the UPF is not reconfigured and the SMF is not restarted",
+                    "‘System.Threading.ThreadAbortException: Thread was being aborted’ while using Response.Redirect"
+            );
+
             for(int i = 0; i < SPECIALIZATION_COUNT; i++) {
                 Specialization s = new Specialization(UserSpecialization.values()[i]);
                 specializations.add(s);
@@ -106,7 +127,7 @@ public class GenerateMockData {
 
             for(int i = 0; i < ALERT_COUNT; i++) {
                 Alert a = Alert.builder()
-                        .description("Example description")
+                        .description(exampleDescriptions.get(i% exampleDescriptions.size()))
                         .boxIp("10.0.8." + PRNG.nextInt(254))
                         .severity(
                                 AlertSeverity.values()[PRNG.nextInt(10) % AlertSeverity.values().length]
