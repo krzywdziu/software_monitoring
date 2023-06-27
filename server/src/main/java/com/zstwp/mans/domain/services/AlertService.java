563090d028ffe6d073c727a40e65dc3d970c3b48
mrospond <marekrospond@gmail.com>
2023-06-27 23:21:07 +0200

    feat: send email when manually assigned

--- server/src/main/java/com/zstwp/mans/domain/services/AlertService.java
+++ server/src/main/java/com/zstwp/mans/domain/services/AlertService.java
@@ -17,6 +17,7 @@ public class AlertService {
 
     private final AlertRepository alertRepository;
     private final UserService userService;
+    private final EmailService emailService;
 
     public List<Alert> getAllAlerts() {
         return alertRepository.findAll();
@@ -48,6 +49,9 @@ public class AlertService {
     }
 
     public Integer assignAlertToUser(long alertId, long userId) {
+        var user = userService.getUserById(userId);
+        var alert = getAlertById(alertId);
+        emailService.sendEmail(user.getEmail(), " New alert!", alert.getDescription());
         return alertRepository.assignAlertToUser(alertId, userService.getUserById(userId));
     }
 
